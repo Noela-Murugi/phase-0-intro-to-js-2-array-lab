4484eb3a5bba3320c94b1e2c4b52fcd384d185aa
Noela-Murugi <murugin254@gmail.com>
2022-08-17 13:36:07 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+// Write your solution here!
+
+let cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+      cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+      cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+      cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+      cats.shift();
+}
+
+//appendCat
+function appendCat(name){
+      return [...cats, name];
+}
+
+//prependCat
+function prependCat(name){
+      return [name, ...cats];
+}
+
+//remove
+function removeLastCat(){
+      return cats.slice(0, -1);
+}
+function removeFirstCat(){
+      return cats.slice(1);
+}
